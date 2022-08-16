9d9a788467adcedacb01878f7237cfa29dccd4a6
lucasansei <lucas.ansei@eokoe.com>
2022-08-16 06:53:02 -0300

    Removing logs for now

--- src/api/routes/twitter-webhook.js
+++ src/api/routes/twitter-webhook.js
@@ -1936,7 +1936,7 @@ router.post('/twitter-webhook', async (req, res) => {
                             return n.code === quick_reply;
                         });
                         next_node = next_node[0];
-                        console.log(next_node);
+                        // console.log(next_node);
                         const analytics_post = await analytics_api.post_analytics(stash.conversa_id, next_node.code, stash.current_node, stash.first_msg_tz, 1, undefined, 'DURING_DECISION_TREE');
                         const analytics_id = analytics_post.data.id;
 
@@ -2242,7 +2242,7 @@ router.post('/twitter-webhook', async (req, res) => {
                         else if (metadata.is_questionnaire_end) {
 
                             const node = flow.nodes[1];
-                            console.log(node);
+                            // console.log(node);
                             const new_stash = {
                                 current_node: flow.nodes[0].code,
                                 started_at: Date.now(),
@@ -2663,7 +2663,7 @@ router.post('/twitter-webhook', async (req, res) => {
             }
             else {
                 // Começando coversa
-                console.log(flow);
+                // console.log(flow);
                 const node = flow.nodes[0];
                 const stash = {
                     current_node: flow.nodes[0].code,
